301f7fdc2cf6b16d8d0f635fa74a2d27b6c05643
ShuntaYamazaki <yamazaki.s.ap@m.titech.ac.jp>
2024-12-08 17:31:25 +0900

    code refactoring

--- nba_api_utils/input_data.py
+++ nba_api_utils/input_data.py
@@ -4,19 +4,23 @@ from nba_api_utils.team import Team
 import streamlit as st
 from datetime import timedelta
 
-def input_season_and_date():
-    # シーズンと日付の入力
-    season = st.text_input("Enter Season (e.g., 2023-24):", "")
+def input_season():
+    # シーズンの入力
+    season = st.text_input("シーズンを選択する (e.g., 2023-24):", "")
     if not season:
         st.stop()
 
-    date = st.date_input("Enter Game Date (YYYY-MM-DD):")
+    return season
+
+def input_date():
+    # 日付の入力
+    date = st.date_input("日付を選択する (YYYY-MM-DD):")
     if not date:
         st.stop()
     date = date - timedelta(days=1)  # Adjust date to the day before
     formatted_date = date.strftime("%Y-%m-%d")
 
-    return season, formatted_date, date
+    return formatted_date, date
 
 def select_game_from_date(season, formatted_date):
     # 試合情報の取得
@@ -24,51 +28,52 @@ def select_game_from_date(season, formatted_date):
     games_on_date = game.game_log
 
     if games_on_date.empty:
-        st.warning("No games found for the given date.")
+        st.warning(f"{formatted_date}に行われた試合のデータは存在しません。")
         st.stop()
 
     # 試合選択
     game_options = {row["MATCHUP"]: row["GAME_ID"] for _, row in games_on_date.iterrows()}
     filtered_keys = [key for key in game_options.keys() if "vs" in key]
-    selected_game = st.selectbox("Select a game:", filtered_keys)
+    selected_game = st.selectbox("試合を選択する:", filtered_keys)
     if not selected_game:
         st.stop()
 
     return game, game_options[selected_game], selected_game
 
-def select_team_and_player(game, game_id):
+def select_team(game, game_id):
     # チーム選択
     teams = game.get_teams(game_id)
     selected_team = st.selectbox("Select a team:", teams)
     if not selected_team:
         st.stop()
 
+    return selected_team
+
+def select_player(game, game_id, selected_team):
     # プレイヤー選択
     players_on_team = game.get_player_names(game_id, selected_team)
     selected_player_name = st.selectbox("Select a player:", players_on_team)
     if not selected_player_name:
         st.stop()
 
-    return selected_team, selected_player_name
+    return selected_player_name
 
 def select_player_by_game():
-    season, formatted_date, date = input_season_and_date()
+    season = input_season()
+    formatted_date, date = input_date()
     game, game_id, selected_game = select_game_from_date(season, formatted_date)
-    selected_team, selected_player_name = select_team_and_player(game, game_id)
+    selected_team = select_team(game, game_id)
+    selected_player_name = select_player(game, game_id, selected_team)
 
     player = Player(selected_player_name)
 
     return game_id, player.id, season, selected_player_name, selected_game, date
 
 def select_game():
-    season, formatted_date, date = input_season_and_date()
+    season = input_season()
+    formatted_date, date = input_date()
     game, game_id, selected_game = select_game_from_date(season, formatted_date)
-
-    # チーム選択
-    teams = game.get_teams(game_id)
-    selected_team = st.selectbox("Select a team:", teams)
-    if not selected_team:
-        st.stop()
+    selected_team = select_team(game, game_id)
 
     team = Team(selected_team)
 
